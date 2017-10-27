09a1feb4a3cd60cf05f6ca2a7e9dc629e0901c69
Cecilia Wong <ceciliakhwong@gmail.com>
2017-10-27 15:22:53 -0400

    Refactor to remove initial steps of finding unique candidates

--- exercise.rb
+++ exercise.rb
@@ -6,33 +6,31 @@ ballots = [{1 => 'Smudge', 2 => 'Tigger', 3 => 'Simba'},
            {1 => 'Smudge', 2 => 'Simba', 3 => 'Felix'}]
 
 # get list of unique candidate names
-unique_candidates = []
-
-ballots.each do |ballot|
-  unique_candidates << ballot.values
-end
-
-unique_candidates.flatten!.uniq!
+# unique_candidates = []
+#
+# ballots.each do |ballot|
+#   unique_candidates << ballot.values
+# end
+#
+# unique_candidates.flatten!.uniq!
 
 # create hash of candidates and their points
-candidate_points = {}
+candidate_points = Hash.new(0)
 
-unique_candidates.each do |candidate|
-  candidate_points[candidate] = 0
-end
+# unique_candidates.each do |candidate|
+#   candidate_points[candidate] = 0
+# end
 
 # iterate through ballots and assign points to candidates
 ballots.each do |ballot|
   ballot.each do |rank, name|
     if rank == 1
-      points = 3
+      candidate_points[name] += 3
     elsif rank == 2
-      points = 2
+      candidate_points[name] += 2
     else
-      points = 1
+      candidate_points[name] += 1
     end
-
-    candidate_points[name] += points
   end
 end
 
